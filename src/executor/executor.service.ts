116ef2943b1649d450f809195c692a6e1dee88bc
Leon Prouger <leonprou@gmail.com>
2024-05-01 16:51:43 +0300

    Onboarding task improvements for Fuse Network

--- src/executor/executor.service.ts
+++ src/executor/executor.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { WalletService } from '../wallet/wallet.service'; // Adjust the import path as necessary
-import { BaseWallet, JsonRpcProvider, parseEther, Provider, SigningKey } from 'ethers';
+import { BaseWallet, JsonRpcProvider, parseEther, parseUnits, Provider, SigningKey } from 'ethers';
 
 @Injectable()
 export class ExecutorService {
@@ -8,7 +8,7 @@ export class ExecutorService {
   private provider: Provider;
 
   constructor(private walletService: WalletService) {
-    console.log(process.env.PROVIDER_URL);
+    console.log(`Using RPC endpoint: ${process.env.PROVIDER_URL}}`);
     this.provider = new JsonRpcProvider(process.env.PROVIDER_URL)
     this.depositAccount = new BaseWallet(new SigningKey(process.env.DEPOSIT_ACCOUNT_PRIVATE_KEY), this.provider);
   }
@@ -17,23 +17,26 @@ export class ExecutorService {
     // Implementation of task execution
     console.log(`Executing task: ${taskName}`);
     if (taskName === 'create') {
-    return `Task ${taskName} executed successfully.`;
+      return `Task ${taskName} executed successfully.`;
     }
   }
 
   async executeOnboard(groupId: string) {
     const taskName = 'onboard';
-    const depositAmount = parseEther('0.00001');
+    const depositAmount = parseEther(process.env.DEPOSIT_AMOUNT);
     // Implementation of task execution
     console.log(`Executing task: ${taskName}`);
+    console.log(`Deposit account address: ${this.depositAccount.address}`);
     const wallets = await this.walletService.getWalletsByGroup(groupId);
     let nonce = await this.provider.getTransactionCount(this.depositAccount.address);
     for (const wallet of wallets) {
-      console.log(`Transferring funds to wallet: ${wallet.address}`);
+      console.log(`Transferring funds to wallet: ${wallet.address}, using nonce: ${nonce}`);
       const txResponse = await this.depositAccount.sendTransaction({
         to: wallet.address,
         value: depositAmount,
-        nonce: nonce
+        nonce: nonce,
+        gasPrice: process.env.GAS_PRICE || undefined,
+        gasLimit: 21000
       });
       console.log(`Transaction hash: ${txResponse.hash}`);
       nonce++;
