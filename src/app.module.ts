9024c700bc078f53dc71d2ff1434f785e1610491
Leon Prouger <leonprou@gmail.com>
2024-07-24 11:02:39 +0300

    add health endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HealthModule } from './health/health.module';
 import { WalletModule } from './wallet/wallet.module';
 import { ExecutorModule } from './executor/executor.module';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -10,9 +11,11 @@ import { CommandsModule } from './commands/commands.module';
 import { EngineModule } from './engine/engine.module';
 import { GasPriceModule } from './queries/gas-price/gas-price.module';
 import { BlockchainProviderModule } from './utils/blockchain-provider/blockchain-provider.module';
+import { HealthController } from './health/health.controller';
 
 @Module({
   imports: [
+    HealthModule,
     WalletModule, ExecutorModule, BlockchainProviderModule,
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URI),
@@ -20,9 +23,10 @@ import { BlockchainProviderModule } from './utils/blockchain-provider/blockchain
     CommandsModule,
     EngineModule,
     GasPriceModule,
-    BlockchainProviderModule
+    BlockchainProviderModule,
+    HealthModule
   ],
-  controllers: [AppController],
+  controllers: [AppController, HealthController],
   providers: [AppService],
 })
 export class AppModule { }
